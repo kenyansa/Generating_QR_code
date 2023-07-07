97549325fb1e8b7d594bb05aebc4f551c6f568f2
Kenyansa <kenyansajustine@gmail.com>
2023-07-07 10:00:46 +0300

    add event listener to the buttonfor submit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const form = document.getElementById("generate_form");
 const qrCode = document.getElementById("qrcode");
 
+const onGenerateSubmit = (e)=>{
+    e.preventDefault();
+
+    const url = document.getElementById("url").value;
+    const size = document.getElementById("size").value;
+
+    console.log(url, size);
+
+}
+form.addEventListener('submit', onGenerateSubmit);
