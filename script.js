7045631c5f7d2fb3708d3539de780b74ea29ebd8
Kenyansa <kenyansajustine@gmail.com>
2023-07-07 11:34:13 +0300

    create a save button for the image generated

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const qrCode = document.getElementById("qrcode");
 
 const onGenerateSubmit = (e)=>{
     e.preventDefault();
+    clearUI();
 
     const url = document.getElementById("url").value;
     const size = document.getElementById("size").value;
@@ -10,11 +11,21 @@ const onGenerateSubmit = (e)=>{
     if (url === ""){
              alert("Please enter a URL");
     }else{
-        showSpinner();
+        showSpinner(); //show spinner for 1 sec
         setTimeout(()=>{
             hideSpinner();
+            generateQRCode(url, size);
         }, 1000)
     }
+}
+
+const generateQRCode = (url, size)=>{
+    const qrcode = new QRCode('qrcode', {
+        text: url,
+        width: size,
+        height: size,
+        colorDark: "blue",
+    })
 }
 
 const showSpinner = () =>{
@@ -23,6 +34,21 @@ const showSpinner = () =>{
 const hideSpinner = () =>{
     document.getElementById("spinner").style.display = "none"
 }
+
+const clearUI = ()=>{
+    qrCode.innerHTML = "";
+};
+
+const createSaveBtn = (saveUrl)=>{
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList = 'bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5'
+    link.href = saveUrl;
+    link.download = 'qrcode';
+    link.innerHTML = 'Save Image';
+    document.getElementById('generated');
+}
+
 hideSpinner();
 
 form.addEventListener('submit', onGenerateSubmit);
