5cd387ab81a151a5fed9616634ad8cdeb4ff1e73
Kenyansa <kenyansajustine@gmail.com>
2023-07-07 10:40:12 +0300

    create a timeout of 1sec for the loading stuff

--- script.js
+++ script.js
@@ -7,7 +7,22 @@ const onGenerateSubmit = (e)=>{
     const url = document.getElementById("url").value;
     const size = document.getElementById("size").value;
 
-    console.log(url, size);
-
+    if (url === ""){
+             alert("Please enter a URL");
+    }else{
+        showSpinner();
+        setTimeout(()=>{
+            hideSpinner();
+        }, 1000)
+    }
 }
+
+const showSpinner = () =>{
+    document.getElementById("spinner").style.display = "block"
+}
+const hideSpinner = () =>{
+    document.getElementById("spinner").style.display = "none"
+}
+hideSpinner();
+
 form.addEventListener('submit', onGenerateSubmit);
